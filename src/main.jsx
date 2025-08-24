132eb27c0250e725ca4bef4d02671df8920bf00b
Le Duong <leduongnewmemes123@gmail.com>
2025-08-24 16:26:25 +0700

    feat: Configured basic routing for components

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import Root from './Root';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router";
+
+
+let router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+          index: true,
+          element: <Home />,
+      },
+      {
+          path: 'shop',
+          element: <Shop />,
+      },
+      {
+          path: 'cart',
+          element: <Cart />,
+      },
+    ]
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <RouterProvider router={router} />,
 )
