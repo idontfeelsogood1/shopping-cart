067b68106f5731fae7feeaadc580128760f2f4a5
Le Duong <leduongnewmemes123@gmail.com>
2025-08-25 16:11:22 +0700

    feat: Finished routing structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,10 @@
 import { createRoot } from 'react-dom/client'
 import Root from './Root';
+import Home from './components/Home/Home';
+import Shop from './components/Shop/Shop';
+import Cart from './components/Cart/Cart';
+import ErrorPage from './components/ErrorPage/ErrorPage'
+
 import {
   createBrowserRouter,
   RouterProvider,
