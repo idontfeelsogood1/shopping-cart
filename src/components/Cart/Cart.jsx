1da103ce9705312abe090ac0604502d69fd89f43
Le Duong <leduongnewmemes123@gmail.com>
2025-08-25 16:12:56 +0700

    feat: Finish component

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,59 @@
+import { useOutletContext, Link } from "react-router"
+import CartItem from "../CartItem/CartItem"
+
+function Cart() {
+    const [addedItems, setAddedItems] = useOutletContext()
+    const mapArray = Array.from(addedItems.entries())
+    let totalPrice = 0
+
+    for (let entries of mapArray) {
+        for (let item of entries[1]) {
+            totalPrice += item.price
+        }
+    }
+
+    function handleCheckout() {
+        setAddedItems(new Map())
+    }
+
+    return (
+        <main>
+            <h1>Your Cart</h1>
+            <section className="container">
+                <h2>Cart Items: {addedItems.size}</h2>
+                <div className="item-container">
+                    {mapArray.map((id) => {
+                        return <CartItem
+                            id={id}
+                        />
+                    })}
+                </div>
+            </section>
+            <footer className="payment">
+                <h2>Order Summary</h2>
+                <section className="pricing-details">
+                    <div className="space-between">
+                        <span>Subtotal</span>
+                        <span>${totalPrice}</span>
+                    </div>
+                    <div className="space-between">
+                        <span>Shipping</span>
+                        <span>Free</span>
+                    </div>
+                </section>
+                <section className="checkout">
+                    <div className="space-between">
+                        <span>Total</span>
+                        <span>${totalPrice}</span>
+                    </div>
+                    <div className="button-group">
+                        <button type="button" onClick={handleCheckout}>Proceed to Checkout</button>
+                        <Link to={{pathname: "/shop"}}>Continue Shopping</Link>
+                    </div>
+                </section>
+            </footer>
+        </main>
+    )
+}
+
+export default Cart
